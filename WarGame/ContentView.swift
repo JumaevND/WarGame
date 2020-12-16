b9944e6539be8f348ee537dd745b5454113c9f5b
NuriddinJumaev <nuriddin.jumaev98>
2020-12-16 15:35:40 +0500

    SwiftUI Project 1

--- WarGame/ContentView.swift
+++ WarGame/ContentView.swift
@@ -8,9 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    //variable
+    @State private var runNum1 = 2
+    @State private var runNum2 = 3
+    @State private var scorePlayer = 0
+    @State private var scoreCPU = 0
+    
+    //ui
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            //background
+            Image("background")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+            //logo
+                Spacer()
+                Image("logo")
+                Spacer()
+            //cards
+                HStack{
+                    Spacer()
+                    Image("card" + String(runNum1))
+                    Spacer()
+                    Image("card" + String(runNum2))
+                    Spacer()
+                }
+            //deal button
+                Spacer()
+                Button(action: {
+                
+                    //Ramdomize
+                    self.runNum1 = Int.random(in: 2...14)
+                    self.runNum2 = Int.random(in: 2...14)
+                    
+                    //Update score
+                    if self.runNum1 > self.runNum2 {
+                        self.scorePlayer += 1
+                    }else if self.runNum2 > self.runNum1{
+                        self.scoreCPU += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton").renderingMode(.original)
+
+                })
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                //Player data
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(scorePlayer))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                //CPU data
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(scoreCPU))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            
+        }
     }
 }
 
